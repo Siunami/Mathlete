e00f849da7a5d2f124ffe4f8b9b135ebdc436f96
Matthew <siunami.matt@gmail.com>
2015-12-28 23:31:54 -0800

    Intial upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var first = (Math.floor(Math.random()* 10) + 1);
+var second = (Math.floor(Math.random()* 10) + 1);
+var operation = Math.rand(["subtract", "add", "multiply", "divide"]);
+console.log(operation);
+
+var problem  = document.getElementById("problem").nodeValue;
+
+var answer = first
+console.log(answer);
