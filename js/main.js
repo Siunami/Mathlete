f21906215a872efb2643b826af157cb77b4b182c
Matthew <siunami.matt@gmail.com>
2015-12-29 22:38:20 -0800

    finshed initial javascript

--- js/main.js
+++ js/main.js
@@ -1,36 +1,78 @@
-var operation = ["subtract", "add", "multiply", "divide"];
-var randomOperation = Math.floor(Math.random() * operation.length); //selects a random array position
-var selector = operation[randomOperation]; //a string value equal to randomOperation value
-var first = (Math.floor(Math.random()* 10) + 1);
-var second = (Math.floor(Math.random()* 10) + 1);
-console.log(first);
-console.log(second);
-console.log(selector);
+var operation = ["subtract", "add", "multiply"];
+var user, randomOperation, selector, first, second, answer;
+var score = 0;
 
-function onClick(){
-  if (selector == "add")
-    add();
-  else if (selector == "subtract")
-    subtract();
-  else if (selector == "multiply")
-    multiply();
-  else if (selector == "divide")
-    divide();
+function newProblem(){
+  randomOperation = Math.floor(Math.random() * operation.length); //selects a random array position
+  selector = operation[randomOperation]; //a string value equal to randomOperation value
+  first = (Math.floor(Math.random()* 10) + 1);
+  second = (Math.floor(Math.random()* 10) + 1);
+  problem = [
+    {problem: first + " - " + second + " = "},
+    {problem: first + " + " + second + " = "},
+    {problem: first + " x " + second + " = "},
+  ]
+  document.getElementById("problem").innerHTML = problem[randomOperation].problem;
+  return randomOperation, selector, first, second, answer;
 }
 
+function enterpressalert(e, text){
+var code = (e.keyCode ? e.keyCode : e.which);
+  if(code == 13) { //Enter keycode
+    if (selector == "add")
+      add();
+    else if (selector == "subtract")
+      subtract();
+    else if (selector == "multiply")
+      multiply();
+    //I want to avoid floating point numbers for now
+    //else if (selector == "divide")
+    //  divide();
+  }
+}
 function add(){
-  var answer = first + second;
-  console.log(answer);
+  user = document.getElementById("user").value;
+  answer = first + second;
+  console.log("user: " + user);
+  console.log("addition answer: " + answer);
+  if (user == answer) {
+    document.getElementById('user').value = ""
+    score = score + 1
+    document.getElementById("score").innerHTML = "Score : " + score;
+    newProblem();
+  } else {
+    console.log("you fucked up");
+  }
 }
 function multiply(){
-  var answer = first * second;
-  console.log(answer);
+  user = document.getElementById("user").value;
+  answer = first * second;
+  console.log("user: " + user);
+  console.log("multiplication answer: " + answer);
+  if (user == answer) {
+    document.getElementById('user').value = ""
+    score = score + 1
+    document.getElementById("score").innerHTML = "Score : " + score;
+    newProblem();
+  } else {
+    console.log("you fucked up");
+  }
 }
 function subtract(){
-  var answer = first - second;
-  console.log(answer);
+  var user = document.getElementById("user").value;
+  answer = first - second;
+  console.log("user: " + user);
+  console.log("addition answer: " + answer);
+  if (user == answer) {
+    document.getElementById('user').value = ""
+    score = score + 1
+    document.getElementById("score").innerHTML = "Score : " + score;
+    newProblem();
+  } else {
+    console.log("you fucked up");
+  }
 }
-function divide(){
-  var answer = first / second;
-  console.log(answer);
-}
+//function divide(){
+//  var answer = first / second;
+//  console.log(answer);
+//}
