96fa4b00a578e13d73858b27c1d830e3bae4f2f2
Matthew <siunami.matt@gmail.com>
2016-01-03 17:59:09 -0800

    Changed random number range depending on operation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
-var operation = ["subtract", "add", "multiply"];
+var operation = ["subtract", "add", "multiply", "divide"];
 var user, randomOperation, selector, first, second, answer;
 var score = 0;
 
-window.onLoad = newProblem();
+window.addEventListener("load", function(){
+  newProblem();
+  document.getElementById("user").focus();
+});
 
 function newProblem(){
   randomOperation = Math.floor(Math.random() * operation.length); //selects a random position of operation array
   selector = operation[randomOperation]; //a string value equal to randomOperation value
-  first = (Math.floor(Math.random()* 10) + 1);
-  second = (Math.floor(Math.random()* 10) + 1);
+  if (selector == "add") {
+    first = (Math.floor(Math.random()* 100) + 1);
+    second = (Math.floor(Math.random()* 100) + 1);
+  }
+
+  if (selector == "subtract") {
+    first = (Math.floor(Math.random()* 100) + 1);
+    second = (Math.floor(Math.random()* 100) + 1);
+  }
+  if (selector == "multiply") {
+    first = (Math.floor(Math.random()* 12) + 1);
+    second = (Math.floor(Math.random()* 12) + 1);
+  }
+  if (selector == "divide"){
+    first = (Math.floor(Math.random()* 144) + 1);
+    second = (Math.floor(Math.random()* 144) + 1);
+    while (((first % second) != 0) || (first == second) || ((first / second) == 2) || (second == 1)) {
+      first = (Math.floor(Math.random()* 144) + 1);
+      second = (Math.floor(Math.random()* 144) + 1);
+    }
+  }
   problem = [
     {problem: first + " - " + second + " = "},
     {problem: first + " + " + second + " = "},
     {problem: first + " x " + second + " = "},
+    {problem: first + " / " + second + " = "},
   ]
   document.getElementById("problem").innerHTML = problem[randomOperation].problem; //grab correct operation syntax from problem object
-  return randomOperation, selector, first, second, answer;
+  return randomOperation, selector, first, second;
 }
 function enterpressalert(e, text){
 var code = (e.keyCode ? e.keyCode : e.which);
@@ -26,9 +49,8 @@ var code = (e.keyCode ? e.keyCode : e.which);
       subtract();
     else if (selector == "multiply")
       multiply();
-    //I want to avoid floating point numbers for now
-    //else if (selector == "divide")
-    //  divide();
+    else if (selector == "divide")
+      divide();
   }
 }
 function add(){
@@ -51,6 +73,21 @@ function multiply(){
   answer = first * second;
   console.log("user: " + user);
   console.log("multiplication answer: " + answer);
+  if (user == answer) {
+    document.getElementById('user').value = "";
+    score = score + 1
+    document.getElementById("score").innerHTML = "Score : " + score;
+    correct();
+    newProblem();
+  } else {
+    wrong();
+  }
+}
+function divide(){
+  user = document.getElementById("user").value;
+  answer = first / second;
+  console.log("user: " + user);
+  console.log("division answer: " + answer);
   if (user == answer) {
     document.getElementById('user').value = "";
     score = score + 1
@@ -84,7 +121,3 @@ function correct(){
   document.getElementById('body').className = "correct";
   var myVar =  setInterval(function(){  document.getElementById('body').className = ""; clearInterval(myVar);}, 100);
 }
-//function divide(){
-//  var answer = first / second;
-//  console.log(answer);
-//}
